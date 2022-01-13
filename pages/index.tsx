bea9ece673507a7d2612d1fa3459b235abe94a1c
ydergachev <uriy.dergachev@gmail.com>
2022-01-13 20:04:14 +0300

    Implement tag and p

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Htag, Button } from "../components";
+import { Htag, P, Tag, Button } from "../components";
 
 export default function Home(): JSX.Element {
   return (
@@ -6,6 +6,13 @@ export default function Home(): JSX.Element {
       <Htag tag="h3">Текст</Htag>
       <Button appearance='primary' arrow="right">Кнопка</Button>
       <Button appearance='ghost'>Кнопка</Button>
+      <P size="l">Большой</P>
+      <P size="m">Средний</P>
+      <P size="s">Маленький</P>
+      <Tag size="s">Маленький</Tag>
+      <Tag size="m" color="red">Маленький</Tag>
+      <Tag size="s" color="green">Маленький</Tag>
+
     </div>
   );
 }

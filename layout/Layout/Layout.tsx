397af47883e6d53aea566b526fd54a07beb6d7a7
ydergachev <uriy.dergachev@gmail.com>
2022-01-14 21:36:20 +0300

    drop export

--- layout/Layout/Layout.tsx
+++ layout/Layout/Layout.tsx
@@ -6,7 +6,7 @@ import { Header } from "../Header/Header";
 import { Sidebar } from "../Sidebar/Sidebar";
 import { Footer } from "../Footer/Footer";
 
-export const Layout = ({ children }: LayoutProps): JSX.Element => {
+const Layout = ({ children }: LayoutProps): JSX.Element => {
   return (
     <>
       <Header />
